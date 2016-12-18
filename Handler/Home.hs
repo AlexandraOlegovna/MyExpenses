ac2933d2f5211360c2725875b51ccf726c717613
AlexandraOlegovna <nikolenko.rnd@gmail.com>
2016-12-18 13:56:16 +0300

    Add route to index.html

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -17,49 +17,52 @@ data FileForm = FileForm
 -- The majority of the code you will write in Yesod lives in these handler
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
-getHomeR :: Handler Html
-getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
 
-    let submission = Nothing :: Maybe FileForm
-        handlerName = "getHomeR" :: Text
+-- getHomeR :: Handler Html
+getHomeR :: Handler ()
+getHomeR = sendFile "text/html" "static/index.html"
+-- getHomeR = do
+--     (formWidget, formEnctype) <- generateFormPost sampleForm
+--
+--     let submission = Nothing :: Maybe FileForm
+--         handlerName = "getHomeR" :: Text
+--
+--     defaultLayout $ do
+--         let (commentFormId, commentTextareaId, commentListId) = commentIds
+--         aDomId <- newIdent
+--         setTitle "Welcome To Yesod!"
+--         $(widgetFile "homepage")
 
-    defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
 
-
-postHomeR :: Handler Html
-postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
-
-    defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
-
-sampleForm :: Form FileForm
-sampleForm = renderBootstrap3 BootstrapBasicForm $ FileForm
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField textSettings Nothing
-    -- Add attributes like the placeholder and CSS classes.
-    where textSettings = FieldSettings
-            { fsLabel = "What's on the file?"
-            , fsTooltip = Nothing
-            , fsId = Nothing
-            , fsName = Nothing
-            , fsAttrs =
-                [ ("class", "form-control")
-                , ("placeholder", "File description")
-                ]
-            }
-
-commentIds :: (Text, Text, Text)
-commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
+-- postHomeR :: Handler Html
+-- postHomeR = do
+--     ((result, formWidget), formEnctype) <- runFormPost sampleForm
+--     let handlerName = "postHomeR" :: Text
+--         submission = case result of
+--             FormSuccess res -> Just res
+--             _ -> Nothing
+--
+--     defaultLayout $ do
+--         let (commentFormId, commentTextareaId, commentListId) = commentIds
+--         aDomId <- newIdent
+--         setTitle "Welcome To Yesod!"
+--         $(widgetFile "homepage")
+--
+-- sampleForm :: Form FileForm
+-- sampleForm = renderBootstrap3 BootstrapBasicForm $ FileForm
+--     <$> fileAFormReq "Choose a file"
+--     <*> areq textField textSettings Nothing
+--     -- Add attributes like the placeholder and CSS classes.
+--     where textSettings = FieldSettings
+--             { fsLabel = "What's on the file?"
+--             , fsTooltip = Nothing
+--             , fsId = Nothing
+--             , fsName = Nothing
+--             , fsAttrs =
+--                 [ ("class", "form-control")
+--                 , ("placeholder", "File description")
+--                 ]
+--             }
+--
+-- commentIds :: (Text, Text, Text)
+-- commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
---
+++ static/vue_template.js
@@ -0,0 +1,86 @@
+Vue.component('demo-grid', {
+  template: '#grid-template',
+  props: {
+    data: Array,
+    columns: Array,
+    filterKey: String
+  },
+  data: function () {
+    var sortOrders = {}
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1
+    })
+    return {
+      sortKey: '',
+      sortOrders: sortOrders
+    }
+  },
+  computed: {
+    filteredData: function () {
+      var sortKey = this.sortKey
+      var filterKey = this.filterKey && this.filterKey.toLowerCase()
+      var order = this.sortOrders[sortKey] || 1
+      var data = this.data
+      if (filterKey) {
+        data = data.filter(function (row) {
+          return Object.keys(row).some(function (key) {
+            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+          })
+        })
+      }
+      if (sortKey) {
+        data = data.slice().sort(function (a, b) {
+          a = a[sortKey]
+          b = b[sortKey]
+          return (a === b ? 0 : a > b ? 1 : -1) * order
+        })
+      }
+      return data
+    }
+  },
+  filters: {
+    capitalize: function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
+  }
+})
+
+// bootstrap the demo
+window.onload = function () {var demo = new Vue({
+  el: '#demo',
+  data: {
+    searchQuery: '',
+    gridColumns: ['type', 'theme', 'item', 'cost', 'date'],
+    gridData: [
+      { type: '+', theme: 'Shop', item: "none", cost: 100.00, date: '12.08.2016'},
+      { type: '+', theme: 'School', item: "none", cost: 120.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'},
+      { type: '+', theme: 'Appartment', item: "none", cost: 1000.00, date: '12.08.2016'}
+
+
+    ]
+  }
+});
+}
